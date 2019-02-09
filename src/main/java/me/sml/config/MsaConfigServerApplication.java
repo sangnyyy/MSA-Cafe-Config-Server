118586e6dbb08963d65189c6d303e1ed0e18dfa5
SML <dltkdals2202@gmail.com>
2019-02-10 00:57:01 +0900

    feature : project added
    Signed-off-by: SML <dltkdals2202@gmail.com>

---
+++ src/main/java/me/sml/config/MsaConfigServerApplication.java
@@ -0,0 +1,14 @@
+package me.sml.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaConfigServerApplication.class, args);
+    }
+
+}
+
