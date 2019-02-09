9cfd7d108f0aa43f90bc420f91ecc4a52a4fa037
SML <dltkdals2202@gmail.com>
2019-02-10 01:17:23 +0900

    feat : config server 관련 설정 적용
    Signed-off-by: SML <dltkdals2202@gmail.com>

--- src/main/java/me/sml/config/Application.java
+++ src/main/java/me/sml/config/Application.java
@@ -2,12 +2,14 @@ package me.sml.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class MsaConfigServerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsaConfigServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
